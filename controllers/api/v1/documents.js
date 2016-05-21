d9295c088b1a9c705da9856f2eafd936a6e08596
iustin.bulimar <iustin.bulimar@gmail.com>
2016-05-21 23:04:22 +0300

    Returnes response to upload

--- controllers/api/v1/documents.js
+++ controllers/api/v1/documents.js
@@ -8,21 +8,50 @@ exports.upload = function(req, res) {
   Document.find().exec((err, documents) => {
     if (err) res.status(400).send(err);
 
-    let arraybuffer = req.file.buffer;
-    let data = new Uint8Array(arraybuffer);
-    let arr = new Array();
-    for(let i = 0; i != data.length; ++i) 
-      arr[i] = String.fromCharCode(data[i]);
-    let bstr = arr.join("");
+    let data = new Uint8Array(req.file.buffer);
+    let array = new Array();
+    for(let i = 0; i !== data.length; ++i) {
+      array[i] = String.fromCharCode(data[i]);
+    }
+    let bstr = array.join("");
    
     let workbook = XLSX.read(bstr, {type:"binary"});
-    let first_sheet_name = workbook.SheetNames[0];
-    let worksheet = workbook.Sheets[first_sheet_name];
+    let firstSheetName = workbook.SheetNames[0];
+    let worksheet = workbook.Sheets[firstSheetName];
 
-    let jsons = XLSX.utils.sheet_to_json(worksheet);
+    let range = worksheet['!ref'];
+    // console.log(range);
+    let startRow = parseInt(range[1]);
+    let endRow = parseInt(range[4]);
+    let startColumn = range[0].charCodeAt(0);
+    let endColumn = range[3].charCodeAt(0);
 
-    console.log(jsons);
+    let values = [];
+    if(endRow == 2) {
+      for(let column = startColumn; column <= endColumn; ++column) {
+        let keyAddress = String.fromCharCode(column) + 1;
+        let valAddress = String.fromCharCode(column) + 2;
+        values.push({ key: worksheet[keyAddress].v, 
+                      val: worksheet[valAddress].v });
+      }
+    } else {
+      for(let column = startColumn + 1; column <= endColumn; ++column) {
+        let keyAddress = String.fromCharCode(column) + 1;
+        let val = [];
+        for(let row = startRow + 1; row <= endRow; ++row) {
+          let subKeyAddress = String.fromCharCode(startColumn) + row;
+          let valAddress = String.fromCharCode(column) + row;
+          val.push({ key: worksheet[subKeyAddress].v, 
+                     val: worksheet[valAddress].v });
+        }
+        values.push({ key: worksheet[keyAddress].v, 
+                      val: val });
+      }
+    }
 
-    res.json({ documents: documents });
+    console.log(values);
+
+    res.json({ id: "1234",
+               values: values });
   })
-};
+};  
